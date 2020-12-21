931f5c4c7f6b6c0ef38ddecbac949c6d31643647
mikatpt <mikatpt@gmail.com>
2020-12-20 23:35:26 -0500

    Added authentication, currently testing it!

--- pages/typing.js
+++ pages/typing.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import { generateWords } from '../utils/Logic';
 import styles from '../styles/Typing.module.css';
@@ -12,11 +13,18 @@ export async function getServerSideProps() {
 To do:
   - Head section with metadata
   - Add a <main> tag, perhaps?
+  - Stats component
+  - Navigation component
 */
 export default function TypeTwo({ word }) {
+  const [session] = useSession();
+  console.log(session);
+
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Welcome to TypeTwo!</h2>
+      {!session && <>Not signed in <br /> <button onClick={signIn} type="button">Sign in</button></>}
+      {session && <>Signed in as {session.user.email} <br /> <button onClick={signOut} type="button">Sign out</button></>}
       <Typer word={word} />
     </div>
   );
