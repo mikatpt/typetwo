b33053513d76e3811a47ffec325327e393f57f21
mikatpt <mikatpt@gmail.com>
2020-12-22 03:16:57 -0500

    Added very basic per-round metrics

--- components/RoundMetrics.js
+++ components/RoundMetrics.js
@@ -1,3 +1,17 @@
-export default function RoundMetrics() {
+import PropTypes from 'prop-types';
 
+export default function RoundMetrics({ wpm, errors, acc }) {
+  return (
+    <div>
+      Words per minute: {wpm}
+      Errors: {errors}
+      Accuracy: {acc}%
+    </div>
+  );
 }
+
+RoundMetrics.propTypes = {
+  wpm: PropTypes.number.isRequired,
+  errors: PropTypes.number.isRequired,
+  acc: PropTypes.number.isRequired,
+};
