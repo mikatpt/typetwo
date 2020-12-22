8665463f2c0a1a696484fbf3d24859f2e4475aaf
mikatpt <mikatpt@gmail.com>
2020-12-22 00:39:12 -0500

    Fixed email authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,14 +8,7 @@ const options = {
       clientSecret: process.env.GOOGLE_SECRET,
     }),
     Providers.Email({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
+      server: process.env.EMAIL_SERVER_URL,
       from: process.env.EMAIL_FROM,
     }),
   ],
