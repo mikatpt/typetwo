eadc9042528ce2c79679fe95d40e9f657ee86770
mikatpt <mikatpt@gmail.com>
2020-12-22 20:52:41 -0500

    Moved typing stuff to home page

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,77 @@
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import Head from 'next/head';
-import Link from 'next/link';
+import { getSession } from 'next-auth/client';
 
-import styles from '../styles/Home.module.css';
+import { getInfo, sendInfo } from '../utils/APILogic';
+import { formatStats, generateWords } from '../utils/Logic';
+import styles from '../styles/Typing.module.css';
 
-// store all chars in outgoing and incoming
-// display only last 20 chars of outgoing and first 20 chars of incoming
+import RoundMetrics from '../components/RoundMetrics';
+import Typer from '../components/Typer';
 
-export default function Home() {
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+  return { props: { word: generateWords(200), session } };
+}
+/*
+To do:
+  - Head section with metadata
+  - Add a <main> tag, perhaps?
+  - Stats component
+  - Navigation component
+  - Maybe move metrics up to _app?
+*/
+
+export default function TypeTwo({ word, session }) {
+  const [metrics, setMetrics] = useState({});
+  const [stats, setStats] = useState([0, 0, 0]);
+  useEffect(() => {
+    if (session) {
+      getInfo(session)
+        .then((res) => {
+          setMetrics(res.data[0]);
+          setStats(formatStats(res.data[0]));
+        });
+    }
+  }, []);
+
+  // metrics: {id, user_id, totalWords, totalTime, fastestWPM,
+  //           lastWPM, lastErrors, lastAccuracy, singles, doubles}
+  // data: [wordList, timeSpent, errors, digraphs, fifths]
+  // Send data to database.
+  const sendData = (data) => {
+    setStats(formatStats(data));
+    if (session) {
+      sendInfo(session, [data, metrics])
+        .then(() => getInfo(session))
+        .then((res) => setMetrics(res.data));
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
         <title>TypeTwo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to TypeTwo!
-        </h1>
-        <div className={styles.grid}>
-          <Link href="/typing">
-            <div className={styles.card}>
-              <h3>Typing App</h3>
-            </div>
-          </Link>
+        <RoundMetrics wpm={stats[0]} errors={stats[1]} acc={stats[2]} />
+        <Typer word={word} sendData={sendData} />
 
-          <Link href="/about">
-            <div className={styles.card}>
-              <h3>About &rarr;</h3>
-            </div>
-
-          </Link>
-        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
+
+TypeTwo.propTypes = {
+  word: PropTypes.string.isRequired,
+  session: PropTypes.shape({ expires: PropTypes.string,
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      email: PropTypes.string,
+      image: PropTypes.string,
+    }) }),
+};
+
+TypeTwo.defaultProps = { session: null };
