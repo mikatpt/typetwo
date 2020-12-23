0ee553aaee07cf7f29b0fbc85491cb013deb1cf4
mikatpt <mikatpt@gmail.com>
2020-12-22 20:52:18 -0500

    Moved static components to top-level

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,30 @@
 /* eslint-disable react/prop-types */
-import '../styles/globals.css';
+import Link from 'next/link';
 import { Provider } from 'next-auth/client';
 
-function MyApp({ Component, pageProps }) {
+import '../styles/globals.css';
+
+import Navigation from '../components/Navigation';
+
+export default function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
+      <Link href="/">
+        <h1 id="title">TypeTwo</h1>
+      </Link>
+      <Navigation {...pageProps} />
+
       <Component {...pageProps} />
+      <footer className="footer">
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        </a>
+      </footer>
     </Provider>
   );
 }
-
-export default MyApp;
