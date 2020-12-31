8a2a40092c696c73dd249b09ede1190af8aa80c3
mikatpt <mikatpt@gmail.com>
2020-12-30 20:02:19 -0500

    Wrote pair graph and implemented the fanciest mouseover functionality you've ever seen

--- components/Statistics/Pairs.js
+++ components/Statistics/Pairs.js
@@ -1,26 +1,15 @@
 import { useEffect, createRef } from 'react';
 import PropTypes from 'prop-types';
-import ReactTooltip from 'react-tooltip';
 
-// import createHistogram from '../../utils/graphs/pairHistogram';
+import createHistogram from '../../utils/graphs/pairHistogram';
 
 export default function Pairs({ pairs }) {
   const ref = createRef();
 
   useEffect(() => {
-    // createHistogram(pairs, ref);
+    createHistogram(ref, pairs);
   }, [pairs]);
 
-  return (
-    <div>
-      <span data-tip data-for="ttpPairs">?</span>
-
-      <div ref={ref} />
-      <ReactTooltip id="ttpPairs" uuid="ttpPairs" type="dark" place="top" effect="solid">Not implemented yet!<br />Doubles</ReactTooltip>
-
-    </div>
-  );
+  return (<div ref={ref} className="svg-container" />);
 }
-Pairs.propTypes = {
-  pairs: PropTypes.object.isRequired,
-};
+Pairs.propTypes = { pairs: PropTypes.object.isRequired };
--- utils/graphs/pairHistogram.js
+++ utils/graphs/pairHistogram.js
@@ -1,17 +1,15 @@
 import * as d3 from 'd3';
 import { calculateWPM } from '../Logic';
 
-const createHistogram = (ref, chars, capital = false) => {
+const createHistogram = (ref, chars) => {
   d3.select(ref.current).selectAll('*').remove();
 
-  const str = capital ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : 'abcdefghijklmnopqrstuvwxyz';
+  const data = Object.keys(chars)
+    .map((pair) => (
+      { pair, total: chars[pair].total, wpm: calculateWPM(chars[pair].total, chars[pair].time) }))
+    .sort((a, b) => (a.wpm <= b.wpm ? -1 : 1));
 
-  const data = str.split('')
-    .map((char) => (chars[char]
-      ? ({ char, wpm: calculateWPM(chars[char].total, chars[char].time), total: chars[char].total })
-      : { char, wpm: 0, total: 0 }));
-
-  // Create canvas
+  // Canvas
   const width = 800;
   const height = 400;
 
@@ -19,59 +17,62 @@ const createHistogram = (ref, chars, capital = false) => {
     .append('svg')
     .attr('width', width)
     .attr('height', height)
-    // .attr('preserveAspectRatio', 'xMinYMin meet')
-    // .attr('viewBox', `0 0 ${width} ${height}`)
     .classed('svg-content-responsive', true);
 
-  // Create chart
+  // Chart
   const margin = 40;
   const chart = canvas.append('g').attr('transform', `translate(${margin}, ${margin})`);
 
-  // Create X and Y axis
+  // X, Y axis
   const x = width - 2 * margin;
   const y = height - 2 * margin;
-
   const max = d3.max(data, (d) => d.wpm);
 
-  const xScale = d3.scaleBand().range([0, x]).domain(data.map((i) => i.char)).padding(0.15);
+  const xScale = d3.scaleBand().range([0, x]).domain(data.map((d) => d.pair)).padding(0.1);
   const yScale = d3.scaleLinear().range([y, 0]).domain([0, max]);
 
+  const xAxis = d3.axisBottom(xScale).tickValues([]);
   const yAxis = d3.axisLeft().scale(yScale).ticks(8).tickValues(d3.range(0, max + 1, max / 8));
 
-  // Append axes to chart
-  chart.append('g').attr('transform', `translate(0, ${y})`).call(d3.axisBottom(xScale));
+  chart.append('g').attr('transform', `translate(0, ${y})`).call(xAxis);
   chart.append('g').call(yAxis);
 
-  // Title the chart
-  const casing = capital ? 'Uppercase' : 'Lowercase';
+  // Axis label
+  canvas.append('text')
+    .attr('x', (x / 2) + margin)
+    .attr('y', height - 10)
+    .attr('text-anchor', 'middle')
+    .text('Pairs');
+
+  // Graph title
   canvas.append('text')
     .attr('x', (x / 2) + margin)
     .attr('y', 20)
     .attr('text-anchor', 'middle')
     .attr('font-weight', 'bold')
-    .text(`Average Speed, ${casing} Letters`);
+    .text('Pair speed (Slow to Fast)');
 
-  // Define tooltip. Allow hovering over tooltip.
+  // Create tooltip
   const tooltip = d3.select(ref.current).append('div').attr('class', 'tooltip');
 
-  const uuid = capital ? 'caps' : 'lowers';
+  // Hovering on OR above bars will display data.
   canvas.on('touchmove mousemove', function b(event) {
     const location = d3.pointer(event, this)[0];
 
     // Bisector returns a func which finds the closest index to a given coordinate
     // Not sure why it doesn't account for margins, so we do so manually
-    const findIdx = d3.bisector((d) => xScale(d.char) + margin).left;
+    const findIdx = d3.bisector((d) => xScale(d.pair) + margin).left;
     const current = findIdx(data, location) - 1;
 
     if (current > -1 && location < width - margin) {
       const d = data[current];
 
       chart.selectAll('rect').attr('opacity', 1);
-      d3.select(`#${uuid}_${current}`).attr('opacity', 0.8);
+      d3.select(`#pair_${current}`).attr('opacity', 0.8);
 
-      tooltip.html(`Char: ${d.char}<br/>Speed: ${d.wpm}wpm<br/>Total: ${d.total}`)
-        .style('left', `${xScale(d.char)}px`)
-        .style('top', `${yScale(d.wpm) - 20}px`)
+      tooltip.html(`Pair: ${d.pair}<br/>Speed: ${d.wpm}wpm<br/>Total: ${d.total}`)
+        .style('left', `${xScale(d.pair)}px`)
+        .style('top', `${yScale(d.wpm) - 30}px`)
         .style('opacity', 1);
     }
   }).on('touchend mouseleave', () => {
@@ -79,15 +80,15 @@ const createHistogram = (ref, chars, capital = false) => {
     tooltip.style('opacity', 0);
   });
 
-  // Append bars to graph
+  // Add bars
   chart.selectAll('rect')
     .data(data).enter()
     .append('rect')
-    .attr('x', (d) => xScale(d.char))
+    .attr('x', (d) => xScale(d.pair))
     .attr('y', (d) => yScale(d.wpm))
     .attr('width', xScale.bandwidth())
     .attr('height', (d) => y - yScale(d.wpm))
     .attr('fill', 'rgb(5, 65, 128)')
-    .attr('id', (d, i) => `${uuid}_${i}`);
+    .attr('id', (d, i) => `pair_${i}`);
 };
 export default createHistogram;
