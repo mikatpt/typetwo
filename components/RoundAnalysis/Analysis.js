7e2ac777253faf693e9b505d2261de23cf4981e7
mikatpt <mikatpt@gmail.com>
2021-01-10 01:39:02 -0500

    Small styling adjustments

--- components/RoundAnalysis/Analysis.js
+++ components/RoundAnalysis/Analysis.js
@@ -6,7 +6,7 @@ import Histogram from './Histogram';
 export default function RoundAnalysis({ stats }) {
   // render analysis only if we have a previous round in memory. Ergo, fifths and words must exist.
   return (stats[3] !== 0 && stats[4]) && (
-    <div style={{ display: 'flex', width: '770px' }}>
+    <div style={{ display: 'flex', width: '711px' }}>
       <Pairs pairs={stats[5]} />
       <Histogram fifths={stats[3]} words={stats[4]} />
     </div>
