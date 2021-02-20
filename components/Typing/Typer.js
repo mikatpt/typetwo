c08ca28308b3653d5328cf7a85b5e33f06e46cf0
mikatpt <mikatpt@gmail.com>
2021-02-19 22:21:43 -0500

    Fixed Typer to batch state updates after detecting some extra re-rendering

--- components/Typing/Typer.js
+++ components/Typing/Typer.js
@@ -6,65 +6,60 @@ import { useKeyPress, modifyEscEnter, modifyBack } from '../../utils/KeyboardLog
 
 import CharacterList from './CharacterList';
 
+const initialState = {
+  current: 0,
+  fifths: [],
+  spaces: 0,
+  data: [],
+  next: 1,
+  errors: {},
+  space: false,
+};
+
 export default function Typer({ words, getWords, prefs, sendData }) {
-  const [current, setCurrent] = useState(0);
-  const [fifths, setFifths] = useState([]);
-  const [spaces, setSpaces] = useState(0);
-  const [data, setData] = useState([]);
+  const [state, setState] = useState(initialState);
   const Time = useRef(new Timer());
   const pref = useRef();
 
-  // Error handling
-  const [next, setNext] = useState(1);
-  const [errors, setError] = useState({});
-  const [space, setSpace] = useState(false);
-
-  const reset = () => {
-    setCurrent(0);
-    setFifths([]);
-    setSpaces(0);
-    setData([]);
-    setNext(1);
-    setError({});
-    setSpace(false);
-  };
+  const reset = () => { setState(initialState); };
 
   useEffect(() => { pref.current = prefs.wordset; }, [prefs]);
   useEffect(() => { reset(); }, [words]);
 
   // Sends data, but times out if you're typing below 20wpm
   useEffect(() => {
-    if (current === words.length) {
+    if (state.current === words.length) {
       const timeSpent = Time.current.end('wpm');
-      const lastFifths = [...fifths, Time.current.end('fifth')];
+      const lastFifths = [...state.fifths, Time.current.end('fifth')];
       const timeout = 60000 / (20 / (words.length / 5));
-      if (timeSpent < timeout) sendData([words, timeSpent, errors, data, lastFifths]);
+      if (timeSpent < timeout) sendData([words, timeSpent, state.errors, state.data, lastFifths]);
       else reset();
     }
-  }, [current]);
+  }, [state.current]);
 
   modifyEscEnter((key) => (key === 'esc' ? reset() : getWords(pref.current)));
   modifyBack(); // See logic file for backspace implementation
 
   // Listens for key presses and checks if correct, with graceful error handling.
   useKeyPress((key) => {
+    let { spaces, space, current, next, data, fifths, errors } = state;
+    let err;
     const pair = current > 0 ? words[current - 1] + words[current] : null;
 
     if (current === 0 && !errors[current]) Time.current.start('all');
 
     if (key === words[current]) {
-      if (pair) setData([...data, [pair, Time.current.end('pair')]]);
+      if (pair) data = [...data, [pair, Time.current.end('pair')]];
 
       if (key === ' ') {
         if (spaces + 1 === Math.ceil(words.split(' ').length / 5)) {
-          setSpaces(0);
-          setFifths([...fifths, Time.current.end('fifth')]);
-        } else setSpaces(spaces + 1);
+          spaces = 0;
+          fifths = [...fifths, Time.current.end('fifth')];
+        } else spaces += 1;
       }
-      const n = current + 1;
-      setCurrent(n);
-      setNext(n + 1);
-      setSpace(false);
+      current += 1;
+      next = current + 1;
+      space = false;
 
       // Error handling
     } else if (errors[current]) {
@@ -72,27 +67,32 @@ export default function Typer({ words, getWords, prefs, sendData }) {
       if (key === words[next]) {
         const conditions = (space && next === current + 3) || (!space && next === current + 2);
         if (conditions && next + 1 < words.length) {
-          setData([...data, [pair, Time.current.end('pair')]]);
-          setCurrent(next + 1);
-        } else if (space && next + 1 < words.length) setNext(next + 1);
-      } else setNext(-1); // ...otherwise, do not permit user to continue until correct key pressed.
+          data = [...data, [pair, Time.current.end('pair')]];
+          current = next + 1;
+        } else if (space && next + 1 < words.length) next += 1;
+      } else next = -1; // ...otherwise, do not permit user to continue until correct key pressed.
     } else if (current !== words.length) {
-      const err = {
+      err = {
         current: words[current],
         prev: words[current - 1] + words[current],
         next: words[current] + words[next],
         word: getWord(words, current),
       };
-      setError({ ...errors, [current]: err });
+      errors = { ...errors, [current]: err };
 
       if (key === words[next]) {
-        if (key === ' ' || words[next + 1] === ' ') setSpace(true);
-        setNext(next + 1);
+        if (key === ' ' || words[next + 1] === ' ') space = true;
+        next += 1;
       }
+    }
+
+    // Only update state if one of the three conditionals was hit.
+    if (current !== state.current || err || next !== state.next) {
+      setState({ current, next, fifths, spaces, data, errors, space });
     }
   });
 
-  return <CharacterList words={words} current={current} errors={errors} />;
+  return <CharacterList words={words} current={state.current} errors={state.errors} />;
 }
 Typer.propTypes = {
   words: PropTypes.string.isRequired,
