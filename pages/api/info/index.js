df7df0b1b581996ce95fa5889c9aea6e1ac8e721
mikatpt <mikatpt@gmail.com>
2021-04-23 02:36:04 -0400

    Env variables

--- pages/api/info/index.js
+++ pages/api/info/index.js
@@ -6,7 +6,7 @@ export default async (req, res) => {
   if (req.method === 'GET') {
     let response;
     try {
-      response = await axios.get('http://localhost:5001/db', config);
+      response = await axios.get(`${process.env.GO_SERVER_URL}/db`, config);
     } catch (e) {
       res.setHeader('Content-Type', 'text/plain');
       console.error(e);
@@ -18,7 +18,7 @@ export default async (req, res) => {
     res.setHeader('Content-Type', 'text/plain');
 
     try {
-      response = await axios.post('http://localhost:5001/db', req.body, config);
+      response = await axios.post(`${process.env.GO_SERVER_URL}/db`, req.body, config);
       res.status(200).send('Successfully saved data.');
     } catch (e) {
       console.error(e.response.statusText);
